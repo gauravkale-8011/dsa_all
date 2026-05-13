9549ad70fc1fadd20807210f1ddb3fe5c69a5461
gauravkale-8011 <kalegaurav593@gmail.com>
2026-05-13 17:02:29 +0530

    Add files via upload

---
+++ sliding_window_Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,11 @@
+s="pwwkew"
+left=0
+seen=set()
+max_len=0
+for right in range(0,len(s)):
+    while s[right] in seen:
+        seen.remove(s[left])
+        left=left+1
+    seen.add(s[right])
+    max_len=max(max_len,right-left+1)
+print(max_len)
